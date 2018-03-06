92c2886e35009754230428d1e6de936312518304
Klaus A <Klaus A@KLAUS>
2018-03-06 20:54:51 +0100

    rebase

---
+++ Lota/LotaRuntime/LotaGame.cs
@@ -0,0 +1,21 @@
+﻿using engenious;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotaRuntime
+{
+    internal class LotaGame : Game
+    {
+        protected override void Initialize()
+        {
+
+        }
+        protected override void OnExiting(object sender, EventArgs e)
+        {
+            base.OnExiting(sender, e);
+        }
+    }
+}
---
+++ Lota/LotaRuntime/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotaRuntime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (LotaGame game = new LotaGame())
+                game.Run(50, 50);
+        }
+    }
+}
